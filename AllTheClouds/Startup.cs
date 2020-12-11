e1a50c0ef36df693ab8c8f6b3556a9741a4bb573
Debbie <1981455+zzjrzz@users.noreply.github.com>
2020-12-11 22:46:10 +1000

    chore(startup): register products services

--- AllTheClouds/Startup.cs
+++ AllTheClouds/Startup.cs
@@ -1,3 +1,4 @@
+using AllTheClouds.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,8 @@ namespace AllTheClouds
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddHttpClient<ProductsService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
