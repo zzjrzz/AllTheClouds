766d89a2e9924df4f956cca320fe050010693c6c
Debbie <1981455+zzjrzz@users.noreply.github.com>
2020-12-16 15:07:16 +1000

    feat(productList): display basic information on products

--- AllTheClouds/ClientApp/src/app/app.module.ts
+++ AllTheClouds/ClientApp/src/app/app.module.ts
@@ -1,31 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import {AppComponent} from './app.component';
+import {NavMenuComponent} from './nav-menu/nav-menu.component';
+import {HomeComponent} from './home/home.component';
+import {FetchDataComponent} from './fetch-data/fetch-data.component';
+import {ProductListComponent} from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'fetch-data', component: FetchDataComponent },
+      {path: '', component: HomeComponent, pathMatch: 'full'},
+      {path: 'fetch-data', component: FetchDataComponent},
+      {path: 'products', component: ProductListComponent},
     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ AllTheClouds/ClientApp/src/app/productSamples.ts
@@ -0,0 +1,16 @@
+export const productSamples = [
+  {
+    'productId': '100AC-001',
+    'name': 'Cuaddes',
+    'description': 'Cuaddes is a test cloud that is fluffy',
+    'unitPrice': 10.11,
+    'maximumQuantity': null
+  },
+  {
+    'productId': '100AC-005',
+    'name': 'Sarusor',
+    'description': 'Sarusor is a bigger meaner cloud but less fluffy',
+    'unitPrice': 68.03,
+    'maximumQuantity': 10
+  }
+];
